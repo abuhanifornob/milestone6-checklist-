5d5c2bf15db96cf2e696e98a5c2a24335e615f91
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-09-01 07:31:05 +0600

    Problem 5 Solved

---
+++ js/problem-4.js
@@ -0,0 +1,6 @@
+// ৪. অনেকগুলা সংখ্যার একটা array হবে। তারপর তোমার কাজ হবে array এর উপরে map ইউজ করে। প্রত্যেকটা উপাদানকে ৭ দিয়ে ভাগ করে ভাগফল আরেকটা array হিসেবে রাখবে। পুরা কাজটা এক লাইনে হবে। 
+
+const numbers = [10, 30, 7, 30, 40, 56, 60, 20, 37];
+
+const newNumbersArray = numbers.map(number => number / 7);
+console.log(newNumbersArray);
